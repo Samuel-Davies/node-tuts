661e20f61de3b2f13ce5048f9a7d8696e28b7cd8
Samuel-Davies <rockydavies21@gmail.com>
2023-02-09 17:07:51 +0000

    refactored the http.js file

--- 10-http.js
+++ 10-http.js
@@ -3,15 +3,16 @@ const http = require('http')
 const server  = http.createServer((req, res)=>{
     if(req.url === '/'){
         res.end('welcome to our homepage!!')
-    }
-    if(req.url === '/about'){
+    } else if(req.url === '/about'){
         res.end('here is our short history')
-    }
-    res.end(`
+    }else
+    {
+        res.end(`
         <h1> Oops!</h1> 
         <p> We can't seem to find the page you'er looking for</p>
         <a href = "/"> back home</a>
         `)
+    }
 })
 
 server.listen(5000)
