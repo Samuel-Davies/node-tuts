1485a218ed93b7609644d1895d8ec216a6f1bba6
Samuel-Davies <rockydavies21@gmail.com>
2023-02-14 16:51:47 +0000

    avoiding callback hell with promises

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const http = require('http')
+const {readFile} = require('fs')
 
-const server = http.createServer((req,res)=>{
-    console.log('request event')
-    res.end('Hello World')
-})
+const getText = (path) =>{
+    return new Promise((resolve, reject)=>{
+        readFile(path, 'utf8', (err, data)=>{
+            if(err){
+                reject(err);
+            }else{
+                resolve(data)
+            } 
+        })
+    })
+}
 
-server.listen(5000, ()=>{
-    console.log('server listening on port : 5000.....')
+getText('./content/first.txt')
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+
+
+readFile('./content/first.txt', 'utf8', (err, data)=>{
+    if(err){
+        return;
+    }else{
+        console.log(data)
+    } 
 })
